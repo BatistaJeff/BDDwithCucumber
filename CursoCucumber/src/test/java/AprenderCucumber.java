ad16a9182a91bc85ba73477d6ba1d72d0840272c
batistajeff <trabalhojbm@gmail.com>
2018-07-21 17:25:37 -0300

    Comit inicial da estrutura do projeto

---
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -0,0 +1,24 @@
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+
+public class AprenderCucumber {
+
+
+@Dado("que criei o arquivo corretamente")
+public void que_criei_o_arquivo_corretamente() {
+	
+}
+
+@Quando("executá-lo")
+public void executá_lo() {
+
+}
+
+@Então("a espeficicação deve finalizar com sucesso")
+public void a_espeficicação_deve_finalizar_com_sucesso() {
+
+}
+
+}
